7c7f3c5f42e07711f9673b876be459207427d5e1
Екатерина Демина <111079583+Ekaterina2709@users.noreply.github.com>
2022-10-25 21:43:38 +0300

    Задача 4

---
+++ 2 py 4.py
@@ -0,0 +1,17 @@
+# 4 – Задайте список из N элементов, заполненных числами 
+# из промежутка [-N, N]. 
+# Найдите произведение элементов на указанных позициях. 
+# Позиции хранятся в файле file.txt в одной строке одно число.
+
+from random import Random, randint
+N = int(input('Введите число N: '))
+rnd =[]
+new = []
+for i in range(N):
+    rnd.append(randint(-N,N+1))
+print(f'Сформированный список{rnd}')
+with open('file.txt','r') as fil:
+    for line in fil:
+        if -len(rnd)<= int(line)< len(rnd):
+            new.append(rnd[int(line)])
+print(f'Выбранные элементы по заданным индексам {new}')
